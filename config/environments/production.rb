2676bea1f88d8e400eda63f18e61f613af1892be
TK201_DTB <sprollhrmk@gmail.com>
2020-07-19 08:09:01 +0900

    host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,7 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { :host => 'otohub.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://otohub2020.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
