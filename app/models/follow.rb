eb8f68d2b45e2c3add574965c48bc1bee42fef5b
TK201_DTB <sprollhrmk@gmail.com>
2020-07-20 03:43:40 +0900

    fix follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,5 +3,5 @@ class Follow < ApplicationRecord
 	belongs_to :following_user, class_name: 'User', optional: true
 	belongs_to :artist, optional: true
 	belongs_to :album, optional: true
-	validates_uniqueness_of :user_id, scope: [:artist, :album, :user]
+	validates_uniqueness_of :user_id, scope: [:artist, :album, :following_user]
 end
