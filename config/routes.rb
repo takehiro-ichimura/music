3d31f25db09e2d102d7e1585e51f489fb6b1fa8e
TK201_DTB <sprollhrmk@gmail.com>
2020-07-21 02:08:53 +0900

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,7 @@ Rails.application.routes.draw do
   post 'artists/:artist_id/:id/comment' => 'posts#comment'
   
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  get '/users/sign_out' => 'devise/sessions#destroy'
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
