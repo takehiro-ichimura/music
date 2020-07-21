7d3a835aef13a5f62062190b59d5ec70fe77bc18
TK201_DTB <sprollhrmk@gmail.com>
2020-07-21 18:04:01 +0900

    improve meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
       title:       "オトハブ|誰でも音楽レビューできるSNS。音楽ブログの宣伝にも。",
       description: "オトハブでは音楽レビューを誰でもかけます。有名なアーティストから自作アルバムまで、熱いレビューをお待ちしています。
       また、音楽ブログを運営している方はリンクを貼って宣伝していただけます。",
-      keywords:    "音楽,ブログ,レビュー,ロック,HIPHOP",
+      keywords:    "音楽,ブログ,レビュー,ロック,HIPHOP,OTOHUB",
       iicon: [
         { href: image_url('favicon.ico') },
         { href: image_url('favicon.ico'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
